1d0f560d1973a5eb63bd596436f0374c4e2e0138
Sayan80bayev <sayan123serv@gmail.com>
2024-06-04 23:08:39 +0500

    register and login pages

--- front-end/src/components/auth/Login.js
+++ front-end/src/components/auth/Login.js
@@ -1,4 +1,40 @@
 import React from "react";
+
 export default function Login() {
-  return <>Hello login</>;
+  return (
+    <div className="form-auth">
+      <div className="wrapper">
+        <div className="title">
+          <span>Login Form</span>
+        </div>
+        <form action="#">
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Password </label>
+          </div>
+          <div className="pass">
+            <a href="#">Forgot password?</a>
+          </div>
+          <button className="btn btn-primary">Login</button>
+          <div className="signup-link">
+            Not a member? <a href="#">Signup now</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- front-end/src/components/auth/Register.js
+++ front-end/src/components/auth/Register.js
@@ -1,4 +1,47 @@
 import React from "react";
+
 export default function Register() {
-  return <>Hello register</>;
+  return (
+    <div className="form-auth">
+      <div className="wrapper">
+        <div className="title">
+          <span>Registration Form</span>
+        </div>
+        <form action="#">
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Username</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              type="password"
+              class="form-control"
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <label for="floatingInput">Password </label>
+          </div>
+
+          <button className="btn btn-primary">Register</button>
+          <div className="signup-link">
+            Already registered? <a href="#">Login </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
