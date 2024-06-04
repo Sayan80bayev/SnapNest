a9cc4417a6e7324e5c6900bb8b1259e0d00262ea
Sayan80bayev <sayan123serv@gmail.com>
2024-06-04 21:52:31 +0500

    login, register start

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+  return <RouterProvider router={routes} />;
 }
-
-export default App;
---
+++ front-end/src/components/auth/Login.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Login() {
+  return <>Hello login</>;
+}
